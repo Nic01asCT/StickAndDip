7f41580c6b83fea91115374125a1234113a9ab40
Nicolas Castillo Thöni <nca135582@stud.gibb.ch>
2023-12-01 20:49:18 +0100

    i accually have no clue of what im doing

--- server/public/script.js
+++ server/public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
 myVideo.muted = true
-const peers = {}
+const peerConnections = {}
 
 navigator.mediaDevices.getDisplayMedia({
     video: true,
@@ -16,48 +16,30 @@ navigator.mediaDevices.getDisplayMedia({
         ]
     }
 
-    const myPeerConnection = new RTCPeerConnection(configuration)
-    stream.getTracks().forEach(track => myPeerConnection.addTrack(track, stream))
-
     socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream, myPeerConnection, configuration);
+        const peerConnection = createPeerConnection(configuration, stream)
+        peerConnections[userId] = peerConnection
+        handlePeerConnection(peerConnection, userId)
     })
 
     socket.on('user-disconnected', userId => {
-        if (peers[userId]) {
-            peers[userId].close()
-            delete peers[userId]
+        if (peerConnections[userId]) {
+            peerConnections[userId].close()
+            delete peerConnections[userId]
         }
     })
-
-    myPeerConnection.onicecandidate = event => {
-        if (event.candidate) {
-            socket.emit('ice-candidate', { candidate: event.candidate, to: userId })
-        }
-    }
-
-    myPeerConnection.ontrack = event => {
-        const userVideo = document.createElement('video')
-        userVideo.srcObject = event.streams[0]
-        userVideo.autoplay = true
-        userVideo.playsinline = true
-        videoGrid.append(userVideo)
-    }
 
     socket.emit('join-room', ROOM_ID)
 })
 
 socket.on('ice-candidate', ({ candidate, from }) => {
-    const peerConnection = peers[from]
+    const peerConnection = peerConnections[from]
     if (peerConnection) {
         peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
     }
 })
 
-function connectToNewUser(userId, stream, myPeerConnection, configuration) {
-    const peerConnection = new RTCPeerConnection(configuration)
-    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream))
-
+function handlePeerConnection(peerConnection, userId) {
     peerConnection.onicecandidate = event => {
         if (event.candidate) {
             socket.emit('ice-candidate', { candidate: event.candidate, to: userId })
@@ -72,17 +54,18 @@ function connectToNewUser(userId, stream, myPeerConnection, configuration) {
         videoGrid.append(userVideo)
     }
 
-    myPeerConnection.createOffer().then(offer => {
-        myPeerConnection.setLocalDescription(new RTCSessionDescription(offer))
-        peerConnection.setRemoteDescription(new RTCSessionDescription(offer))
+    peerConnection.createOffer().then(offer => {
+        peerConnection.setLocalDescription(new RTCSessionDescription(offer))
 
-        peerConnection.createAnswer().then(answer => {
-            peerConnection.setLocalDescription(new RTCSessionDescription(answer))
-            myPeerConnection.setRemoteDescription(new RTCSessionDescription(answer))
-        })
+        // Send the offer to the other user
+        socket.emit('offer', { offer, to: userId })
     })
+}
 
-    peers[userId] = peerConnection
+function createPeerConnection(configuration, stream) {
+    const peerConnection = new RTCPeerConnection(configuration)
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream))
+    return peerConnection
 }
 
 function addVideoStream(video, stream) {
